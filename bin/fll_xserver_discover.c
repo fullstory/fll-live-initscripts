0118151cec6598594f01cc5e5bad26aaf62afdb2
Kel Modderman <kel@otaku42.de>
2008-03-03 22:00:46 +0000

    * fll_xserver_discover:   - add debugging cli switch to print out informational messages during     detection

--- bin/fll_xserver_discover.c
+++ bin/fll_xserver_discover.c
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <strings.h>
 #include <dirent.h>
+#include <unistd.h>
 
 #include "pci/pci.h"
 
@@ -21,7 +22,7 @@
  * Eventually the Xorg drivers will export symbols correlating to supported
  * devices for automatic configuration.
  */
-char *lookup_xorg_dvr_for(const char *string)
+char *lookup_xorg_dvr_for(const char *string, int debug)
 {
 	/* vesa is default driver */
 	char *driver = "vesa";
@@ -44,6 +45,8 @@ char *lookup_xorg_dvr_for(const char *string)
 
 		snprintf(filename, sizeof(filename),
 		         "%s%s", XSERVER_PCIIDS_DIR, entry->d_name);
+		if (debug)
+			printf("I: looking for %s in %s\n", string, filename);
 
 		FILE *file;
 		file = fopen(filename, "r");
@@ -55,6 +58,8 @@ char *lookup_xorg_dvr_for(const char *string)
 				/* printf("%s: %s (match)\n", filename, string); */
 				/* found string in $driver.ids */
 				driver = entry->d_name;
+				if(debug)
+					printf("I: found %s in %s\n", string, driver);
 
 				/* strip .ids extenstion */
 				ptr = strrchr(driver, '.');
@@ -74,18 +79,37 @@ char *lookup_xorg_dvr_for(const char *string)
  * Simple adaptation of example.c from pciutils. Find first VGA class device
  * and export some information about the device that a shell script can source.
  */
-int main(void)
+int main(int argc, char *argv[])
 {
 	struct pci_access *pacc;
 	struct pci_dev *dev;
 	char devbuf[128];
 	char str[9];
+	int opt;
+	int debug = 0;
+
+	while ((opt = getopt(argc, argv, "d")) != -1) {
+		switch(opt) {
+		case 'd':
+			debug++;
+			break;
+		default:
+			break;
+		}
+	}
 
 	pacc = pci_alloc();
 	pci_init(pacc);
 	pci_scan_bus(pacc);
 
 	for (dev = pacc->devices; dev; dev = dev->next) {
+		if (debug)
+			printf("%02x:%02x.%d vendor=%04x device=%04x class=%04x %s\n",
+	     		       dev->bus, dev->dev, dev->func, dev->vendor_id,
+			       dev->device_id, dev->device_class,
+			       pci_lookup_name(pacc, devbuf, sizeof(devbuf),
+			                       PCI_LOOKUP_VENDOR | PCI_LOOKUP_DEVICE,
+			                       dev->vendor_id, dev->device_id));
 		if (dev->device_class == VGA_CLASS) {
 			/* convert bus:dev.func into BusID */
 			printf("XBUSID='PCI:%d:%d:%d'\n",
@@ -106,10 +130,11 @@ int main(void)
 			         dev->vendor_id, dev->device_id);
 
 			/* search for string in xserver pciids lists */
-			printf("XMODULE='%s'\n", lookup_xorg_dvr_for(str));
+			printf("XMODULE='%s'\n", lookup_xorg_dvr_for(str, debug));
 
 			/* only do one VGA device */
-			break;
+			if (!debug)
+				break;
 		}
 	}
 
