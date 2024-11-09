cf1f610a7c9887fedabf56d6df06b701c651e284
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2024-11-09 23:33:10 +0100

    build mail update for Kel
    Signed-off-by: Stefan Lippers-Hollmann <s.l-h@gmx.de>

--- bin/fll_fstab_generator.c
+++ bin/fll_fstab_generator.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2010  Kel Modderman <kel@otaku42.de>
+ *  Copyright (C) 2010  Kel Modderman <kelvmod@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
